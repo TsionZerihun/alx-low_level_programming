64bf0aace2607a65df6eb273e0d36f2f608c03dd
Tsion Zerihun <tsion902@gmail.com>
2022-04-08 15:35:28 -0700

    coiting bety space modifications

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,8 +3,7 @@
 #include <stdio.h>
 
 /**
- * main - Prnits a random number and states if its 
- *        positive, negative or zero
+ * main - Prnits a random number and states if its positive,negative, zero
  *
  * Return: Always 0.
  */
@@ -15,7 +14,7 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-	
+
 if (n > 0)
 printf("%d is positive\n", n);
 else if (n < 0)
