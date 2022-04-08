a1ae1b5444518647e03e7f4eb5b6f137721259fb
Tsion Zerihun <tsion902@gmail.com>
2022-04-07 18:42:09 -0700

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-/*main -entry point*/
+/**
+ *main -entry point
+ *
+ *Return: 0 after printing the function
+ */
 int main(void)
-/* function to print */
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
