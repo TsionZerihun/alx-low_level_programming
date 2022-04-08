786092cdc1c8b738dc4db48f4827e4b7c346d42e
Tsion Zerihun <tsion902@gmail.com>
2022-04-08 16:02:12 -0700

    renaing dogot to digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
-printf("Last dogot of %d is %d %s\n", n, n % 10,
+printf("Last digit of %d is %d %s\n", n, n % 10,
 ((n % 10) == 0) ? "and is zero"
 : (((n % 10) > 5) ? "and is greater than 5"
 : "and is less than 6 and one 0"));
