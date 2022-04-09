6600636a9cfd6a996c7648af0741c7a10efc1d98
Tsion Zerihun <tsion902@gmail.com>
2022-04-08 18:08:58 -0700

    printin 1 to nine without using char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: number one to ten without using variable char
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+
+for ( i = 0; i <= 9; ++i)
+putchar('0' + i);
+
+putchar('\n');
+
+return (0);
+}
