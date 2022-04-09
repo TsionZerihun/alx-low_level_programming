07d709aa41239600ccceabbc974b41486a1b4538
Tsion Zerihun <tsion902@gmail.com>
2022-04-08 18:45:33 -0700

    last task

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print all sing;e digit combination
+ * Return: 0
+ */
+int main(void)
+{
+int c = 0;
+
+while(c < 10)
+{
+putchar(48 + c);
+if (c != 9)
+{
+putchar(',');
+putchar(' ');
+}
+c++;
+}
+putchar('\n');
+return(0);
+}
