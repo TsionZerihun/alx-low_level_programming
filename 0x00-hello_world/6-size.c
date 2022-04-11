83a015aaa585a5fa5227d173414564b94ce04981
Tsion Zerihun <tsion902@gmail.com>
2022-04-11 05:46:31 -0700

    modifiying file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 /**
- *
  *main - Entry point
- *
+ * rints the size of various types on the computer it is compiled and run on.
  * Return: Always 0 (Success)
  */
 int main(void)
