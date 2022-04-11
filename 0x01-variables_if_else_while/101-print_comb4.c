12472f6c625abaa64a6b2754d3daeed39dcc0f87
Tsion Zerihun <tsion902@gmail.com>
2022-04-11 14:48:19 -0700

    11th task

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,9 +16,9 @@ for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 {
 for (digit3 = digit2 + 1; digit3 < 10; digit3++)
 {
-putchar('0' + i);
-putchar('0' + j);
-putchar('0' + k);
+putchar('0' + digit1);
+putchar('0' + digit2);
+putchar('0' + digit3);
 
 if (digit1 != 7 || digit2 != 8 || digit3 != 9)
 {
