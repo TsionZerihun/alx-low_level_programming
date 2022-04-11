b89e43b1803ffb80595bf93bb658652a22045667
Tsion Zerihun <tsion902@gmail.com>
2022-04-11 15:17:00 -0700

    making beety style changes

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,7 +19,7 @@ for (x2 = 0; x2 < 10; x2++)
 {
 for (y1 = x1; y1 < 10; y1++)
 {
-for (y2 = ((x1 ==y1) ?(x2 + 1) : 0); y2 < 10; y2++)
+for (y2 = ((x1 == y1) ? (x2 + 1) : 0); y2 < 10; y2++)
 {
 putchar('0' + x1);
 putchar('0' + x2);
@@ -27,7 +27,7 @@ putchar(' ');
 putchar('0' + y1);
 putchar('0' + y2);
 
-if (x1 !=9 || x2 != 8 || y1 != 9 || y2 != 9)
+if (x1 != 9 || x2 != 8 || y1 != 9 || y2 != 9)
 {
 putchar(',');
 putchar(' ');
