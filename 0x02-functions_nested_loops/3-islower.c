9b22791cdf8f2049705a6004eb77b0ca9b3afda7
Tsion Zerihun <tsion902@gmail.com>
2022-04-12 19:54:07 -0700

    t3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * -islower - return 1 if lowecase and o otherwise
+ *  @c: takes in  a character
+ *  Return: o for upper, 1 for lower
+ */
+int _lower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return(1);
+}
+else
+return(0);
+}
+}
