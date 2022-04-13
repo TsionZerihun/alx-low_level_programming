f5bea7681f44d45db06ce7cedd988ade9b29dce0
Tsion Zerihun <tsion902@gmail.com>
2022-04-13 02:02:01 -0700

    returns 0 if alphabers esle returns 0

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
  * _isalpha.c - checks for alphabetic charater
- * Returns: 1 if c is a lower or uppercase letter.0 if c is not a letter
+ * @c: takes in a character
+ * Return: 1 if c is a lower or uppercase letter.0 if c is not a letter
  */
 int _isalpha(int c)
 {
-if ((c <= 'a' && c <= 'z') || (c >= 'A' || c <= 'Z'))
+if ((c >= 'a' && c <= 'z') || (c >= 'A' || c <= 'Z'))
 {
 return (1);
 }
