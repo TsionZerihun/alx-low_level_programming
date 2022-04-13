82032a43a2b0de847f4e2909ad75e2c955290fdc
Tsion Zerihun <tsion902@gmail.com>
2022-04-13 02:50:26 -0700

    copmute the absolute value on interger

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**_abs: computes absolute value of interger
+ * @n: integer to be computed
+ * Return: absolute vallue of the interger.
+ */
+int _abs(int n)
+{
+if (n >=0)
+return (n);
+}
+else
+return (-n);
+}
