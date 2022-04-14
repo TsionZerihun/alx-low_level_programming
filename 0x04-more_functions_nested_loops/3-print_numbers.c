026537f7b02a2b9299417ee308715de72759d0c2
Tsion Zerihun <tsion902@gmail.com>
2022-04-14 16:09:15 -0700

    task3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * print_numbers - prints number 0 to 9
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+
+int i;
+
+i = 0;
+
+for (i = 0; i <=9; i++)
+
+_putchar(i);
+
+_putchar('\n');
+}
