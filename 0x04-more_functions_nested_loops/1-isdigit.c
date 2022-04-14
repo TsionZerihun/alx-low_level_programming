0ff88d6bcebd26408bd38fb55341bad5fbd5b576
Tsion Zerihun <tsion902@gmail.com>
2022-04-14 15:30:25 -0700

    checking id char is digit from 0-9

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * _isdigit - checks if its digit from 0 to 9
+ * @c: takes in a digit
+ * Return: 1 if its a digit, otherwise 0
+ */
+
+int _isdigit(int c)
+{
+if (c <= 0 && c >= 9)
+{
+return (1);
+}
+else
+return (0)
+}
+}
