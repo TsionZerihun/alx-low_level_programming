22cc4508eb8747bdbfb69c84a5059d3b802850ad
Tsion Zerihun <tsion902@gmail.com>
2022-04-14 04:26:03 -0700

    task 1

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *main - test function that prints if interger is postitive or negativ
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return(0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+tasks for practicing 0x03-debugging lesson
