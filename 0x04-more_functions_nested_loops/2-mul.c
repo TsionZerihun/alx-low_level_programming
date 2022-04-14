5dfdfc7e6d3c228b1dae0f76d0133632a7a03d8b
Tsion Zerihun <tsion902@gmail.com>
2022-04-14 15:58:28 -0700

    tasl 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * mul - mutiply two integers
+ * @a: interger to be passed
+ * @b: interger to be passed
+ * Return: result
+ */
+
+int mul(int a, int b)
+{
+int res;
+res = a * b;
+return res;
+}
