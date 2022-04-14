0fb667db4201c5f82a3214ee5b0b84837178c1be
Tsion Zerihun <tsion902@gmail.com>
2022-04-14 14:42:04 -0700

    function for returning 1 for uppercase letter and zero otherwise

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * _isupper - checks for upper and lowercase letters
+ * Return: 0 if upper 1, 1 if lower
+ */
+
+int _isupper(int c)
+{
+int i = 0;
+
+if (i >= 'A' && i <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
