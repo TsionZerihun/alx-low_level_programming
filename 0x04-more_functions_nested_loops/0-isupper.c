f5b440f4ec210595fe64e168bf0e96cb12701f64
Tsion Zerihun <tsion902@gmail.com>
2022-04-14 15:10:34 -0700

    removing i varialbe in file

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 /**
  * _isupper - checks for upper and lowercase letters
+ *@c: takes in a character
  * Return: 0 if upper 1, 1 if lower
  */
 
 int _isupper(int c)
 {
-int i = 0;
-
-if (i >= 'A' && i <= 'Z')
+if (c >= 'A' && c <= 'Z')
 {
 return (1);
 }
