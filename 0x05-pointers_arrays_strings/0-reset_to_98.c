9523e655a4980a583d94a060f9e13f877e77286e
Tsion Zerihun <tsion902@gmail.com>
2022-04-18 06:40:25 -0700

    commit2

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
-/**
- * reset_to_98 - change the value of integer using pointer
- * @n: pointer
+/** 
+ * reset to_98.c
+ * @n : An integer that will be update
  *
- * Return: void
+ * Return: void 
  */
+
 void reset_to_98(int *n)
 {
-	*n = 98;
+*n = 98;
 }
