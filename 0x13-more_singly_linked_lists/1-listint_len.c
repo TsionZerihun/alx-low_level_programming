7668da33f79c5816428209cbd61160cdb0b51352
Tsion <101357449+TsionZerihun@users.noreply.github.com>
2022-05-25 19:26:39 +0300

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,27 +1,21 @@
-/*
- * File: 0-print_listint.c
- * Auth: Tsi
- */
-
 #include "lists.h"
-#include <stdio.h>
-
 /**
- * print_listint - Prints all the elements of a listint_t list.
- * @h: A pointer to the head of the list_t list.
+ * listint_len - function that returns the number of elements
+ * in a linked listint_t list.
  *
- * Return: The number of nodes in the list_t list.
+ * @h: Parameter Pointer to the head of the linked list
+ *
+ * Return: The number of elements
  */
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
-	size_t nodes = 0;
+	size_t n = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		nodes++;
-		printf("%d\n", h->n);
 		h = h->next;
+		n++;
 	}
 
-	return (nodes);
+	return (n);
 }
